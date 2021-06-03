6462848307582102b4f622244aec6c29e35aae84
demasialol <demasialol@naver.com>
2021-06-03 16:49:55 +0900

    ThreadAndRunnable

--- app/src/main/java/com/androidapp/threadbasic0603/MainActivity.java
+++ app/src/main/java/com/androidapp/threadbasic0603/MainActivity.java
@@ -7,33 +7,74 @@ import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
-    Thread wr;
+    WorkerThread wt;
+    WorkerRunnable wr;
     boolean running = true;
+
+    String TAG = "Thread";
+
+    class WorkerThread extends Thread {
+        public void run() {
+            int i=0;
+            for (i=0; i<20 && running; i++) {
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+
+                }
+                Log.v(TAG, "Thread time " + i);
+            }
+        }
+    }
+    class WorkerRunnable extends Thread {
+        public void run() {
+            int i = 0;
+            for(i=0; i<20 && running; i++) {
+                try {
+                    Thread.sleep(1000);
+                }catch(InterruptedException e){
+                }
+                Log.v(TAG, "Runnable time " + i);
+            }
+        }
+    }
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        @Override
-        protected  void onStart(){
-            super.onStart();
-            running = true;
-            wr = new Thread(new Runnable(){
-                @Override
-                        public void run(){
-                    int i = 0;
-                    for (i = 0; i < 20 && running; i++) {
-                        try {
-                            Thread.sleep(1000);
-                        } catch (InterruptedException e) {
-                        }
-                        Log.v(TAG, "Thread time=" + i);
-                }
-            }
-    });
-            wr.start();
-            Log.v(TAG, "Now I am onStart");
+        Log.v(TAG, "Now I am in onCreate");
+    }
 
 
+    @Override
+    public void onStart(){
+        super.onStart();
+        running = true;
+        wr = new WorkerRunnable();
+        wr.run();
+        wt = new WorkerThread();
+        wt.start();
+        Log.v(TAG,"Now I am in onstart");
+    }
+
+    @Override
+    public void onStop(){
+        super.onStop();
+        running = false;
+        Log.v(TAG,"Now I am in onStop");
+    }
+    @Override
+    public void onPause(){
+        super.onPause();
+        Log.v(TAG,"Now I am in onPause");
+    }
+    @Override
+    public void onResume(){
+        super.onResume();
+        Log.v(TAG,"Now I am in onResume");
+    }
 }
+
